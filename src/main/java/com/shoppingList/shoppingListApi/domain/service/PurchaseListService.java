49a089539b9d1a2bee7f49538c383842b3e71d7f
Hericles Souza <hericles_003@hotmail.com>
2024-06-30 04:03:34 -0300

    feat: create PurchaseListService

---
+++ src/main/java/com/shoppingList/shoppingListApi/domain/service/PurchaseListService.java
@@ -0,0 +1,43 @@
+package com.shoppingList.shoppingListApi.domain.service;
+
+import com.shoppingList.shoppingListApi.domain.dto.item.ItemDTO;
+import com.shoppingList.shoppingListApi.domain.dto.purchaseList.PurchaseListDTO;
+import com.shoppingList.shoppingListApi.domain.dto.purchaseList.PurchaseListRequestDTO;
+import com.shoppingList.shoppingListApi.domain.model.Item;
+import com.shoppingList.shoppingListApi.domain.model.PurchaseList;
+import com.shoppingList.shoppingListApi.domain.repository.PurchaseListRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PurchaseListService {
+    @Autowired
+    private PurchaseListRepository purchaseListRepository;
+
+    @Transactional
+    public PurchaseListDTO create(PurchaseListRequestDTO data) {
+        PurchaseList purchaseList = new PurchaseList();
+        purchaseList.setListName(data.listName());
+        List<Item> items = data.items().stream().map(itemDTO ->
+                        Item.builder()
+                                .name(itemDTO.name())
+                                .quantity(itemDTO.quantity())
+                                .purchaseList(purchaseList)
+                                .build())
+                .toList();
+        purchaseList.setItems(items);
+        PurchaseList savedList = purchaseListRepository.save(purchaseList);
+
+        return this.mapToDTO(savedList);
+    }
+
+    private PurchaseListDTO mapToDTO(PurchaseList purchaseList) {
+        List<ItemDTO> itemDTOs = purchaseList.getItems().stream()
+                .map(item -> new ItemDTO(item.getId(), item.getName(), item.getQuantity()))
+                .toList();
+        return new PurchaseListDTO(purchaseList.getId(), purchaseList.getListName(), itemDTOs);
+    }
+}
