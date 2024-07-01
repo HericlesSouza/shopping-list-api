104676c9c8302dfa1f22a55c9a589ab1adc205d4
Hericles Souza <hericles_003@hotmail.com>
2024-07-01 16:51:12 -0300

    feat: add new validations on DTOs

--- src/main/java/com/shoppingList/shoppingListApi/domain/dto/item/ItemRequestDTO.java
+++ src/main/java/com/shoppingList/shoppingListApi/domain/dto/item/ItemRequestDTO.java
@@ -1,12 +1,12 @@
 package com.shoppingList.shoppingListApi.domain.dto.item;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record ItemRequestDTO(
         @NotBlank(message = "name is required")
         String name,
         @NotNull(message = "quantity is required")
+        @Positive(message = "quantity must be greater than 0")
         Integer quantity
 ) {
 }
--- src/main/java/com/shoppingList/shoppingListApi/domain/dto/purchaseList/PurchaseListRequestDTO.java
+++ src/main/java/com/shoppingList/shoppingListApi/domain/dto/purchaseList/PurchaseListRequestDTO.java
@@ -6,9 +6,11 @@ import com.shoppingList.shoppingListApi.domain.annotation.ArrayListRequired;
 import com.shoppingList.shoppingListApi.domain.dto.item.ItemRequestDTO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record PurchaseListRequestDTO(
         @NotBlank(message = "listName is required")
+        @Pattern(regexp = "^[\\p{L} \\-']+$", message = "listName must contain only letters, spaces, hyphens, and apostrophes")
         String listName,
         @ArrayListRequired
         List<@Valid ItemRequestDTO> items
