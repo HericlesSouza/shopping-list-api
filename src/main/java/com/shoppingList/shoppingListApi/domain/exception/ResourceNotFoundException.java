7f03472e3233a53a5efaa48516eecb235f183b29
Hericles Souza <hericles_003@hotmail.com>
2024-07-01 18:59:59 -0300

    feat(Exception): create default constructor with default message

--- src/main/java/com/shoppingList/shoppingListApi/domain/exception/ResourceNotFoundException.java
+++ src/main/java/com/shoppingList/shoppingListApi/domain/exception/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.shoppingList.shoppingListApi.domain.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
